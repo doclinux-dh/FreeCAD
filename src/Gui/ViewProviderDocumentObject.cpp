9e716a65192db0318824631c4cc3e5a2d08180a3
Zheng, Lei <realthunder.dev@gmail.com>
2019-12-05 11:12:11 +0800

    Gui: add UpdateSelectionVisual view option
    With parameter in Preferences/View/UpdateSelectionVisual, decides
    whether to update selection visual when an object is revealed.

--- src/Gui/ViewProviderDocumentObject.cpp
+++ src/Gui/ViewProviderDocumentObject.cpp
@@ -59,6 +59,7 @@
 #include "ViewProviderExtension.h"
 #include "SoFCUnifiedSelection.h"
 #include "Tree.h"
+#include "ViewParams.h"
 #include <Gui/ViewProviderDocumentObjectPy.h>
 
 FC_LOG_LEVEL_INIT("Gui",true,true)
@@ -208,7 +209,13 @@ void ViewProviderDocumentObject::onChanged(const App::Property* prop)
 
 void ViewProviderDocumentObject::hide(void)
 {
+    auto obj = getObject();
+    if(obj && obj->getDocument() && obj->getNameInDocument())
+        Gui::Selection().updateSelection(
+                false, obj->getDocument()->getName(), obj->getNameInDocument(),0);
+
     ViewProvider::hide();
+
     // use this bit to check whether 'Visibility' must be adjusted
     if (Visibility.testStatus(App::Property::User2) == false) {
         Visibility.setStatus(App::Property::User2, true);
@@ -226,6 +233,13 @@ void ViewProviderDocumentObject::show(void)
         if(getObject())
             getObject()->Visibility.setValue(false);
         return;
+    }
+
+    if(ViewParams::instance()->getUpdateSelectionVisual()) {
+        auto obj = getObject();
+        if(obj && obj->getDocument() && obj->getNameInDocument())
+            Gui::Selection().updateSelection(
+                    true, obj->getDocument()->getName(), obj->getNameInDocument(),0);
     }
 
     // use this bit to check whether 'Visibility' must be adjusted
